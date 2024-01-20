47032ff01c6be4f2e0f008856b512bf6694d6749
Aditya Pentyala <adityapentyala02@gmail.com>
2024-01-20 20:21:20 +0530

    feat: add KMeans clustering method

--- src/clusterer/cluster_embeddings.py
+++ src/clusterer/cluster_embeddings.py
@@ -32,3 +32,18 @@ def create_clusters_Agglomerative(data: list[dict], n_clusters: int=None, distan
         for i in indices:
             data[i]["label"] = unique_label
     return data
+
+def create_clusters_KMeans(data: list[dict], n_clusters: int=8):
+    scaler = StandardScaler
+    embeddings = [face["embedding"] for face in data]
+    scaler.fit(embeddings)
+    scaled_embeddings = scaler.transform(embeddings)
+    clusterer = KMeans(n_clusters=n_clusters)
+    clusterer.fit(scaled_embeddings)
+    labels = clusterer.labels_
+    unique_labels = np.unique(labels)
+    for unique_label in unique_labels:
+        indices = np.where(labels==unique_label)
+        for i in indices:
+            data[i]["label"] = unique_label
+    return data
