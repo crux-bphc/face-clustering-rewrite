da4c1116692f1eb9b4d3a55ffb2173e714d80242
Advik Sinha <advsinha112@gmail.com>
2024-01-21 20:12:22 +0530

    fix: remove dimension of size 1 from embeddings and fix indices error

--- src/clusterer/cluster_embeddings.py
+++ src/clusterer/cluster_embeddings.py
@@ -2,10 +2,11 @@ import pickle
 from sklearn.cluster import DBSCAN, AgglomerativeClustering, KMeans
 from sklearn.preprocessing import StandardScaler
 import numpy as np
+import tensorflow as tf
 
 def create_clusters_DBSCAN(data: list[dict], eps: float=0.5, min_samples: int=10, scale: bool=True):
-    scaler = StandardScaler
-    embeddings = [face["embedding"] for face in data]
+    scaler = StandardScaler()
+    embeddings = [tf.squeeze(face["embedding"]) for face in data]
     scaler.fit(embeddings)
     scaled_embeddings = scaler.transform(embeddings) if scale else embeddings
     clusterer = DBSCAN(eps=eps, min_samples=min_samples)
@@ -13,15 +14,15 @@ def create_clusters_DBSCAN(data: list[dict], eps: float=0.5, min_samples: int=10
     labels = clusterer.labels_
     unique_labels = np.unique(labels)
     for unique_label in unique_labels:
-        indices = np.where(labels==unique_label)
+        indices = np.where(labels==unique_label)[0]
         for i in indices:
             data[i]["label"] = unique_label
     return data, labels, unique_labels
 
 def create_clusters_Agglomerative(data: list[dict], n_clusters: int=None, distance_threshold: float=0.5,
                                   scale: bool=True):
-    scaler = StandardScaler
-    embeddings = [face["embedding"] for face in data]
+    scaler = StandardScaler()
+    embeddings = [tf.squeeze(face["embedding"]) for face in data]
     scaler.fit(embeddings)
     scaled_embeddings = scaler.transform(embeddings) if scale else embeddings
     clusterer = AgglomerativeClustering(n_clusters=n_clusters, distance_threshold=distance_threshold)
@@ -29,14 +30,14 @@ def create_clusters_Agglomerative(data: list[dict], n_clusters: int=None, distan
     labels = clusterer.labels_
     unique_labels = np.unique(labels)
     for unique_label in unique_labels:
-        indices = np.where(labels==unique_label)
+        indices = np.where(labels==unique_label)[0]
         for i in indices:
             data[i]["label"] = unique_label
     return data, labels, unique_labels
 
 def create_clusters_KMeans(data: list[dict], n_clusters: int=8, scale: bool=True):
-    scaler = StandardScaler
-    embeddings = [face["embedding"] for face in data]
+    scaler = StandardScaler()
+    embeddings = [tf.squeeze(face["embedding"]) for face in data]
     scaler.fit(embeddings)
     scaled_embeddings = scaler.transform(embeddings) if scale else embeddings
     clusterer = KMeans(n_clusters=n_clusters)
@@ -44,7 +45,7 @@ def create_clusters_KMeans(data: list[dict], n_clusters: int=8, scale: bool=True
     labels = clusterer.labels_
     unique_labels = np.unique(labels)
     for unique_label in unique_labels:
-        indices = np.where(labels==unique_label)
+        indices = np.where(labels==unique_label)[0]
         for i in indices:
             data[i]["label"] = unique_label
     return data, labels, unique_labels
