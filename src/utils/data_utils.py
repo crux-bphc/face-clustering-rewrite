ce5e76484127355d57a4e9c2430a25c5ef8f9c46
Aditya Pentyala <adityapentyala02@gmail.com>
2024-01-22 23:12:23 +0530

    feat: add names to superfolder images

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -5,13 +5,14 @@ def create_folder_for_clustering(source_dir: str, dest_dir: str):
     os.makedirs(dest_dir)
     n_files = 0
     for root, dirs, files in os.walk(source_dir):
-        for file in files:
-            if file.lower().endswith(('.jpg', '.jpeg', '.png')):
-                source_path = os.path.join(root, file)
-                destination_path = os.path.join(dest_dir, file)
+        for dir in dirs:
+            for file in os.listdir(os.path.join(root,dir)):
+                if file.lower().endswith(('.jpg', '.jpeg', '.png')):
+                    source_path = os.path.join(root,dir,file)
+                    newfile = dir+"_"+file
+                    destination_path = os.path.join(dest_dir, newfile)
 
-                shutil.copy(source_path, destination_path)
-                n_files+=1
-            if n_files%50==0:
-                print(f"Moved {n_files} files!")
-
+                    shutil.copy(source_path, destination_path)
+                    n_files+=1
+                if n_files%100==0:
+                    print(f"Copied {n_files} files!")
