9c2606c704eefa5f8a822015ff3632182c54227e
Aditya Pentyala <adityapentyala02@gmail.com>
2024-01-29 00:19:34 +0530

    feat: add function to find mean and stddev for persons

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import numpy as np
 
 def create_folder_for_clustering(source_dir: str, dest_dir: str):
     os.makedirs(dest_dir)
@@ -15,3 +16,19 @@ def create_folder_for_clustering(source_dir: str, dest_dir: str):
                     n_files+=1
                 if n_files%100==0:
                     print(f"Copied {n_files} files!")
+
+def create_person_attributes(data: list[dict]):
+    sorted_data = sorted(data, key= lambda x: x["filepath"])
+    person_embeddings = {}
+    current_person = sorted_data[0]["filepath"].split("-img-")[0]
+    person_embeddings[current_person] = []
+    for datapoint in sorted_data:
+        person = datapoint["filepath"].split("-img-")[0]
+        if person != current_person:
+            current_person = person
+            person_embeddings[current_person] = []
+        person_embeddings[current_person].append(datapoint["embedding"])
+    person_attributes = {}
+    for person in person_embeddings.keys():
+        person_attributes[person] = [np.mean(person_embeddings[person], axis=0), np.std(person_embeddings[person], axis=0)]
+    return person_attributes
