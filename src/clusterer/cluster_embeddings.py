fb53eb47c5d17da32a7b138639a615b4fe80f2c6
Aditya Pentyala <adityapentyala02@gmail.com>
2024-01-28 23:37:22 +0530

    feat: add metric param to clustering algos

--- src/clusterer/cluster_embeddings.py
+++ src/clusterer/cluster_embeddings.py
@@ -4,12 +4,12 @@ from sklearn.preprocessing import StandardScaler
 import numpy as np
 import tensorflow as tf
 
-def create_clusters_DBSCAN(data: list[dict], eps: float=0.5, min_samples: int=10, scale: bool=True):
+def create_clusters_DBSCAN(data: list[dict], eps: float=0.5, min_samples: int=10, scale: bool=True, metric: str="euclidean"):
     scaler = StandardScaler()
     embeddings = [tf.squeeze(face["embedding"]) for face in data]
     scaler.fit(embeddings)
     scaled_embeddings = scaler.transform(embeddings) if scale else embeddings
-    clusterer = DBSCAN(eps=eps, min_samples=min_samples)
+    clusterer = DBSCAN(eps=eps, min_samples=min_samples, metric=metric)
     clusterer.fit(scaled_embeddings)
     labels = clusterer.labels_
     unique_labels = np.unique(labels)
@@ -24,12 +24,12 @@ def create_clusters_DBSCAN(data: list[dict], eps: float=0.5, min_samples: int=10
     return data, labels, unique_labels, cluster_attributes
 
 def create_clusters_Agglomerative(data: list[dict], n_clusters: int=None, distance_threshold: float=0.5,
-                                  scale: bool=True):
+                                  scale: bool=True, metric: str="euclidean"):
     scaler = StandardScaler()
     embeddings = [tf.squeeze(face["embedding"]) for face in data]
     scaler.fit(embeddings)
     scaled_embeddings = scaler.transform(embeddings) if scale else embeddings
-    clusterer = AgglomerativeClustering(n_clusters=n_clusters, distance_threshold=distance_threshold)
+    clusterer = AgglomerativeClustering(n_clusters=n_clusters, distance_threshold=distance_threshold, metric=metric)
     clusterer.fit(scaled_embeddings)
     labels = clusterer.labels_
     unique_labels = np.unique(labels)
@@ -43,12 +43,12 @@ def create_clusters_Agglomerative(data: list[dict], n_clusters: int=None, distan
         cluster_attributes[unique_label] = [np.mean(cluster_embeddings, axis=0), np.std(cluster_embeddings, axis=0)]
     return data, labels, unique_labels, cluster_attributes
 
-def create_clusters_KMeans(data: list[dict], n_clusters: int=8, scale: bool=True):
+def create_clusters_KMeans(data: list[dict], n_clusters: int=8, scale: bool=True, metric: str="euclidean"):
     scaler = StandardScaler()
     embeddings = [tf.squeeze(face["embedding"]) for face in data]
     scaler.fit(embeddings)
     scaled_embeddings = scaler.transform(embeddings) if scale else embeddings
-    clusterer = KMeans(n_clusters=n_clusters)
+    clusterer = KMeans(n_clusters=n_clusters, metric=metric)
     clusterer.fit(scaled_embeddings)
     labels = clusterer.labels_
     unique_labels = np.unique(labels)
